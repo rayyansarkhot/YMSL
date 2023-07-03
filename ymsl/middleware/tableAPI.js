435403d9609f539d0e56b2ab71150d1dc74debc1
rayyansarkhot <rayyanrsarkhot@gmail.com>
2023-07-03 13:09:41 -0400

    Added functionality so team players auto-generate on teams page

--- ymsl/middleware/tableAPI.js
+++ ymsl/middleware/tableAPI.js
@@ -7,19 +7,26 @@ const port = 3000;
 // Set your API key
 const apiKey = 'AIzaSyARfusRDNwT81WtRtBdlqQ1JyjyoS4YCZM'; // Replace with your API key
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Fetch all data from a Google Sheet
-async function fetchSheetData() {
+async function fetchSheetData(sheetID, data) {
   try {
     // Create the Sheets API client
     const sheets = google.sheets({ version: 'v4' });
 
     // Specify the spreadsheet ID
-    const spreadsheetId = '1QHAQraRDpnUAtPNRQgA-Sl2fHT5VzF7-i3qXuVzOW7c'; // Replace with your spreadsheet ID
+    const spreadsheetId = sheetID; ; // Replace with your spreadsheet ID
 
     // Fetch all data from the spreadsheet
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: 'YMSL Stat Central!A5:Q38', // Replace with your desired sheet name or range
+      range: data, // Replace with your desired sheet name or range
       key: apiKey
     });
 
@@ -34,11 +41,25 @@ async function fetchSheetData() {
   }
 }
 
-// Define the API endpoint
-app.get('/api/sheet-data', async (req, res) => {
+// Define API endpoints
+app.get('/api/leaderboard-data', async (req, res) => {
   try {
     // Fetch the sheet data
-    const sheetData = await fetchSheetData();
+    const sheetData = await fetchSheetData('1QHAQraRDpnUAtPNRQgA-Sl2fHT5VzF7-i3qXuVzOW7c', 'YMSL Stat Central!A5:Q37');
+
+    // Return the data as the API response
+    res.json(sheetData);
+  } catch (error) {
+    // Handle the error
+    res.status(500).json({ error: 'An error occurred while fetching data from the Google Sheet.' });
+  }
+});
+
+// Define the API endpoint
+app.get('/api/team-data', async (req, res) => {
+  try {
+    // Fetch the sheet data
+    const sheetData = await fetchSheetData('1YEcpxahGcLph3EcGJmUBKbVQs7fNS6_LnGFlyAZ2n8g', 'AllPlayersAllGames!A2:B313');
 
     // Return the data as the API response
     res.json(sheetData);
@@ -52,3 +73,5 @@ app.get('/api/sheet-data', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
